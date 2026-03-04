3a9d61e8c805fdf29921ab0edc95151889f05280
danner <mahucom9@gmail.com>
2026-03-03 22:35:09 -0300

    first commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Aula03RH.Models;
+
+namespace Aula03RH
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario   func =  new Funcionario();
+            func.Id = 10;
+            func.Nome = "Neymar";
+            func.Cpf = "12345678910";
+            func.DataAdmissao = DateTime.Parse("01/01/2010");
+            func.Salario = 1000.00M;
+            func.TipoFuncionario = Models.Enuns.TipoFuncionarioEnum.CLT;
+
+            string mensagem = func.ExibirPeriodoExperiencia();
+            Console.WriteLine(mensagem);
+        }
+    }
+}
